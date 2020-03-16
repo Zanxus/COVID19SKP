f05abae3a028e24f2b45974d124f3a59b60a9a1a
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-03-16 14:32:12 +0100

    Fixed Databinding, and added carry over Guesses

--- SKP Mandatory Assignments/CalculatorAndGuessingGame/CalculatorAndGuessingGame/GuessingGameWindow.xaml.cs
+++ SKP Mandatory Assignments/CalculatorAndGuessingGame/CalculatorAndGuessingGame/GuessingGameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,18 +18,38 @@ namespace CalculatorAndGuessingGame
     /// <summary>
     /// Interaction logic for GuessingGameWindow.xaml
     /// </summary>
-    public partial class GuessingGameWindow : Window
+    
+    //Uses INotifyPropertyChanged fire and event when a propertychanges
+    public partial class GuessingGameWindow : Window, INotifyPropertyChanged
     {
 
         int numberToGuess;
-        public int Guesses { get; set; }
+        //The Event that gets fired
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int guesses;
+        //Uses OnPropertyChanged() to change data with the 
+        public int Guesses
+        {
+            get { return guesses; }
+            set { guesses = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Wins { get; set; }
+        public int MyProperty { get; set; }
+
         public GuessingGameWindow()
         {
             //Initializes the number needed to be guessed aswell as number of tries
+            Wins = 0;
             Guesses = 3;
             Random random = new Random();
             numberToGuess = random.Next(0, 10);
+
             InitializeComponent();
+
+            GuessAmountLabel.DataContext = this;
         }
 
         //Adds Return fuctionality to the GuessingGameWindow
@@ -42,15 +63,27 @@ namespace CalculatorAndGuessingGame
         //Handels the Guess logic
         private void Button_Guess(object sender, RoutedEventArgs e)
         {
-            if(Guesses == 0)
-            {
-                
-            }
+
             if(int.TryParse(GuessInput.Text,out int input) && Guesses > 0)
             {
                 Guesses--;
                 OutputText.Content = (numberToGuess > input) ? "Higher" : (numberToGuess < input) ? "Lower" : "Correct";
+                if((string)OutputText.Content == "Correct")
+                {
+                    Wins++;
+                    Guesses += 3;
+                }
             }
+            if (Guesses == 0)
+            {
+                OutputText.Content = $"You Ran out of guesses and guessed {Wins} numbers correctly";
+            }
+        }
+
+        //Updates the value
+        private void OnPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("guesses"));
         }
     }
 }
