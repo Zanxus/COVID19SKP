565cb4380913145d0f1dbffb758dbc96ceb98a16
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-03-16 13:19:23 +0100

    Implimented basic GuessingGame Logic

--- SKP Mandatory Assignments/CalculatorAndGuessingGame/CalculatorAndGuessingGame/GuessingGameWindow.xaml.cs
+++ SKP Mandatory Assignments/CalculatorAndGuessingGame/CalculatorAndGuessingGame/GuessingGameWindow.xaml.cs
@@ -19,8 +19,15 @@ namespace CalculatorAndGuessingGame
     /// </summary>
     public partial class GuessingGameWindow : Window
     {
+
+        int numberToGuess;
+        public int Guesses { get; set; }
         public GuessingGameWindow()
         {
+            //Initializes the number needed to be guessed aswell as number of tries
+            Guesses = 3;
+            Random random = new Random();
+            numberToGuess = random.Next(0, 10);
             InitializeComponent();
         }
 
@@ -30,6 +37,20 @@ namespace CalculatorAndGuessingGame
             MainWindow mainWindow = new MainWindow();
             this.Close();
             mainWindow.Show();
+        }
+
+        //Handels the Guess logic
+        private void Button_Guess(object sender, RoutedEventArgs e)
+        {
+            if(Guesses == 0)
+            {
+                
+            }
+            if(int.TryParse(GuessInput.Text,out int input) && Guesses > 0)
+            {
+                Guesses--;
+                OutputText.Content = (numberToGuess > input) ? "Higher" : (numberToGuess < input) ? "Lower" : "Correct";
+            }
         }
     }
 }
