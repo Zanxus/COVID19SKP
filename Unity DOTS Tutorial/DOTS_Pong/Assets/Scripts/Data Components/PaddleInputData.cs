ed590b1d160d09e95a61203997e2f30ee0fbbed2
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-03-26 13:04:33 +0100

    Added PaddleInputData

---
+++ Unity DOTS Tutorial/DOTS_Pong/Assets/Scripts/Data Components/PaddleInputData.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+
+//Makes it so Component Data can be put onto GameObjects before Entitiy Conversion.
+[GenerateAuthoringComponent]
+public struct PaddleInputData :IComponentData
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+}
