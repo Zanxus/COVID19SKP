cc6d3ba83eb7c1a8631e1b24bb0e749b3b317f46
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-03-27 10:18:49 +0100

    Added BallTag Added SpeedIncreaseOverTimeData Added IncreaseVelocityOverTimeSystem

---
+++ Unity DOTS Tutorial/DOTS_Pong/Assets/Scripts/Data Components/BallTag.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct BallTag : IComponentData
+{
+    //a tag does not contain data, it's only here to tag entities as a way to grab all entities of a given type/tag
+}
---
+++ Unity DOTS Tutorial/DOTS_Pong/Assets/Scripts/Data Components/SpeedIncreaseOverTimeData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct SpeedIncreaseOverTimeData : IComponentData
+{
+    //Default is to name structs varible "Value" if it only contains one, but sometimes this doesn't discribe the variable very well so sometimes it's sercomvented
+    public float increasePerSecond;
+}
