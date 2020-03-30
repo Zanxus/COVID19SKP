86b2848fef1167a389273c2b5f5917b2c0dad4e4
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-03-30 09:42:55 +0200

    Added Ingredent Data Struct Added Pizza Model

---
+++ Week14/Pizzaria/Pizzaria/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzaria
+{
+    class Pizza
+    {
+        public ObservableCollection<Ingredient> Ingredents { get; set; }
+        public decimal Price { get; set; }
+        public enum Size
+        {
+            Normal,Familiy
+        }
+    }
+}
