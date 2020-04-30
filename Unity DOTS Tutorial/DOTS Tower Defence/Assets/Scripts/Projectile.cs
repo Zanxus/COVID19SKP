850742fe5c1f5fb8b0495b988f4ebd5426b62405
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-04-30 14:44:04 +0200

    Added Tower, Projectile class Added IPlaceable Interface Added placeholder Sprites

---
+++ Unity DOTS Tutorial/DOTS Tower Defence/Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    public static Transform pfProjectile;
+    private static void Create()
+    {
+        Instantiate(pfProjectile);
+    }
+}
---
+++ Unity DOTS Tutorial/DOTS Tower Defence/Assets/Scripts/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour, IPlaceable
+{
+    private Vector3 projectileShootFromPosition;
+
+    private void Awake()
+    {
+        projectileShootFromPosition = transform.Find("ProjectileShootFromPosition").position;
+    }
+
+    public void Place()
+    {
+        
+    }
+}
