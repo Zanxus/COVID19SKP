8b18e94f38b3185192dbf357d0fe557654d1850f
Emil Frederiksen <Frederiksen_33@hotmail.com>
2020-05-07 14:43:57 +0200

    Started making Pathfinding (WIP)

--- Unity DOTS Tutorial/DOTS Tower Defence/Assets/Scripts/Pathfinding.cs
+++ Unity DOTS Tutorial/DOTS Tower Defence/Assets/Scripts/Pathfinding.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
-    
+    private Grid<PathNode> grid;
+
+    public Pathfinding(int width,int height)
+    {
+        grid = new Grid<PathNode>(width, height, 10f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+    }
 }
